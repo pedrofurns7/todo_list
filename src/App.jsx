4e44be87fbc53c5fb0afad243590793a8c2513e7
Pedro Fernandes <pedroofraancisco@gmail.com>
2024-09-20 18:21:39 -0300

    [feat] configuracoes iniciais do projeto

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      text: "Estudar useState para implementar no projeto",
+      category: "Estudos",
+      isCompleted: false
+    }, 
+    {
+      id: 3,
+      text: "Ir para o Jiu Jitsu",
+      category: "Pessoal",
+      isCompleted: false
+    }
+
+  ]);
+
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">({todo.category})</p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>X</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+  
+}
+
+export default App
