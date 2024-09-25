450f8661ed4d2f6915238e66be212ee380e7457c
Pedro Fernandes <pedroofraancisco@gmail.com>
2024-09-25 18:36:36 -0300

    [FEAT] add funcionalidades de remove e complete task

--- src/App.jsx
+++ src/App.jsx
@@ -42,16 +42,32 @@ function App() {
     setTodos(newTodos);
   };
 
-  return <div className="app">
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) => todo.id !== id ? todo : null);
+    setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) =>{
+    const newTodos = [...todos];
+    newTodos.map((todo) => 
+      todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
+      
+    );
+    console.log(newTodos);
+    setTodos(newTodos);
+  }
+  return (
+  <div className="app">
     <h1>Lista de Tarefas</h1>
     <div className="todo-list">
       {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
       ))}
     </div>
     <TodoForm addTodo={addTodo} />
   </div>
 
+)
 }
-
 export default App
