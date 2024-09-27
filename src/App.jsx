1eafd69be5ec781a7baa265ede9f8516e8311f27
Pedro Fernandes <pedroofraancisco@gmail.com>
2024-09-27 17:38:00 -0300

    [FEAT] inserindo funcao de busca e filtro de ordenacao

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { useState } from 'react'
 
 import Todo from "./Components/Todo";
 import TodoForm from './Components/TodoForm';
+import Search from './Components/Search';
+import Filter from './Components/Filter';
 
 import "./App.css";
+
+
 
 
 function App() {
@@ -29,6 +33,12 @@ function App() {
 
   ]);
 
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+
+  const [sort, setSort] = useState("Asc");
+
   const addTodo = (text, category) => {
 
     const newTodos = [...todos, {
@@ -48,26 +58,45 @@ function App() {
     setTodos(filteredTodos);
   };
 
-  const completeTodo = (id) =>{
+  const completeTodo = (id) => {
     const newTodos = [...todos];
-    newTodos.map((todo) => 
+    newTodos.map((todo) =>
       todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
-      
+
     );
     console.log(newTodos);
     setTodos(newTodos);
   }
   return (
-  <div className="app">
-    <h1>Lista de Tarefas</h1>
-    <div className="todo-list">
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
-      ))}
-    </div>
-    <TodoForm addTodo={addTodo} />
-  </div>
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
 
-)
+      <div className="todo-list">
+        {todos
+          .filter((todo) => 
+            filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+          .filter((todo) =>
+            todo.text.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo 
+              key={todo.id}     
+              todo={todo}     
+              removeTodo={removeTodo}
+              completeTodo={completeTodo} 
+            />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+
+  )
 }
 export default App
